fb0bd3b3ba080be1d451e0ff571edc2a93529079
Jake Bathman <jake.bathman@gmail.com>
2023-01-27 10:36:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,33 +9,33 @@
 // @namespace https://github.com/jakebathman/Twitch-Channel-Points-Claimer
 // ==/UserScript==
 
-console.log('[AutoClaimer] Started')
+console.log('[AutoClaimer] Started');
 
-const myHeaders = new Headers()
+const myHeaders = new Headers();
 myHeaders.append(
-  'x-access-token',
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJ1c2VySWQiOiIzNzU4N2NmMC1mNjJkLTExZWMtODlhYy1jM2VkMjYxYzI1NmEiLCJzY29wZXMiOlsiKiJdLCJpYXQiOjE2NjQ5OTA2MDMsImlzcyI6InNwb3JlIiwic3ViIjoiMzc1ODdjZjAtZjYyZC0xMWVjLTg5YWMtYzNlZDI2MWMyNTZhIn0.FyJsm7o96CqM0Du0OoH1BjjtNVdcicNTFDNRwmKxPqdAg0nzqmaqwBbg6Nzdz2or-qw9WkDF3c8MhmOd-DEmpA',
-)
-myHeaders.append('x-org-id', '3a0d9a70-d7e7-11ec-8c8a-994d3437ad2d')
+    'x-access-token',
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJ1c2VySWQiOiIzNzU4N2NmMC1mNjJkLTExZWMtODlhYy1jM2VkMjYxYzI1NmEiLCJzY29wZXMiOlsiKiJdLCJpYXQiOjE2NjQ5OTA2MDMsImlzcyI6InNwb3JlIiwic3ViIjoiMzc1ODdjZjAtZjYyZC0xMWVjLTg5YWMtYzNlZDI2MWMyNTZhIn0.FyJsm7o96CqM0Du0OoH1BjjtNVdcicNTFDNRwmKxPqdAg0nzqmaqwBbg6Nzdz2or-qw9WkDF3c8MhmOd-DEmpA'
+);
+myHeaders.append('x-org-id', '3a0d9a70-d7e7-11ec-8c8a-994d3437ad2d');
 
 const raw =
-    '{"query":"mutation ($sourceType: String!) {\\n  watchTimeClaim(input: {sourceType: $sourceType}) {\\n    economyTransactions {\\n      amountId\\n      amountValue\\n      __typename\\n    }\\n    __typename\\n  }\\n}","variables":{"sourceType":"youtube"}}'
+    '{"query":"mutation ($sourceType: String!) {\\n  watchTimeClaim(input: {sourceType: $sourceType}) {\\n    economyTransactions {\\n      amountId\\n      amountValue\\n      __typename\\n    }\\n    __typename\\n  }\\n}","variables":{"sourceType":"youtube"}}';
 
 const requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow',
-}
-function claimPoints () {
-  fetch('https://mycelium.truffle.vip/graphql', requestOptions)
-    .then(response => response.text())
-    .then(result => console.log('[AutoClaimer] result', result))
-    .catch(error => console.log('[AutoClaimer] error', error))
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+};
+function claimPoints() {
+    fetch('https://mycelium.truffle.vip/graphql', requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log('[AutoClaimer] result', result))
+        .catch((error) => console.log('[AutoClaimer] error', error));
 }
 
 // Claim to start
-claimPoints()
+claimPoints();
 
 // Claim points every 5 minutes (plus 5 seconds for buffer)
-setInterval(claimPoints, 5 * 61 * 1000)
+setInterval(claimPoints, 5 * 61 * 1000);
